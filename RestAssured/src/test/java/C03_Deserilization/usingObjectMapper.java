9375c8536d681ff4109da392f4350439d8f5e450
HarishRavichandran1995 <harishravichandran1995@gmail.com>
2025-09-18 21:24:41 +0530

    Deserialization using object mapper

---
+++ RestAssured/src/test/java/C03_Deserilization/usingObjectMapper.java
@@ -0,0 +1,33 @@
+package C03_Deserilization;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class usingObjectMapper {
+
+	 String jsonString = "{\r\n"
+			+ "  \"first_name\" : \"Harish\",\r\n"
+			+ "  \"last_name\" : \"Ravichandran\",\r\n"
+			+ "  \"email\" : \"harishravichandran1995.com\",\r\n"
+			+ "  \"skills\" : [ \"Java\", \"Selenium\" ]\r\n"
+			+ "}";
+	public void objectMapperDeserilization() throws JsonMappingException, JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		C08_Employee employee = mapper.readValue(jsonString, C08_Employee.class);
+		System.out.println(employee.getFirst_name());
+		System.out.println(employee.getLast_name());
+		System.out.println(employee.getEmail());
+		System.out.println(employee.getSkills());
+		
+	}
+	public static void main(String[] args) throws JsonMappingException, JsonProcessingException {
+		// TODO Auto-generated method stub
+	 usingObjectMapper objectName = new usingObjectMapper();
+	 objectName.objectMapperDeserilization();
+	}
+}
